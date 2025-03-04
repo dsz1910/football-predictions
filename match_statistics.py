86f229feaae65d83298f7ca554a2d04fd592f74a
Dawid <dawid.szyjkaa1910@interia.pl>
2025-03-04 15:29:50 +0100

    finished scraping

--- match_statistics.py
+++ match_statistics.py
@@ -1,44 +1,29 @@
 from page_interactor import PageInteractor
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep, perf_counter
+from selenium.common.exceptions import TimeoutException
+from time import perf_counter
 from queue import Queue
 import pandas as pd
 import numpy as np
 import threading
 import pickle
-import functools
 
 
 class ScrapeStatistics(PageInteractor):
     
     lock = threading.Lock()
 
-    def __init__(self, threads_num=3):
+    def __init__(self, threads_num):
         self.matches = self._read_links()
         self.data = []
         self.threads_num = threads_num
         self.task_queue = Queue()
         self.workers = None
 
-    def error_handler(func):
-        @functools.wraps(func)
-        def wrapper(self, *args, **kwargs):    
-            for _ in range(5):
-                try:
-                    result = func(self, *args, **kwargs)
-                    return result
-                except Exception as e:
-                    driver = kwargs['driver']
-                    driver.get(driver.current_url)
-                    print(e)
-                    sleep(1)
-            return None
-        return wrapper
-
     def get_all_stats(self):
         for i, task in enumerate(self.matches):
-            if i % 1000 == 0:
+            if i % 3000 == 0:
                 self.task_queue.put((0, 0))
             self.task_queue.put(task)
 
@@ -57,72 +42,87 @@ class ScrapeStatistics(PageInteractor):
 
         self.save_stats()
         self.save_stats('excel')
+
+    def _scrape_main_stats(self, driver):
+        try:
+            self.wait_until_element_is_visible(driver, By.CLASS_NAME, 'wcl-row_OFViZ')
+        except TimeoutException:
+            return None
+
+        all_stats = self.find_elements(driver, By.CLASS_NAME, 'wcl-row_OFViZ')
+        all_stats = [stats.text.split('\n') for stats in all_stats]
+        match_stats = {stats[1] : (stats[0], stats[2]) for stats in all_stats}
+        return match_stats
         
     def _get_match_stats(self, driver, url, season_idx):
         self.get_website(driver, url)
         final_data = {}
-                
-        sleep(1)
-        if self.is_element_present(driver, By.ID, 'onetrust-reject-all-handler'):
-            self.wait_and_click_button(driver, By.ID, 'onetrust-reject-all-handler')
-
-        self.wait_until_element_is_visible(driver, By.CLASS_NAME, 'wcl-category_ITphf')
-        all_stats = self.find_elements(driver, By.CLASS_NAME, 'wcl-category_ITphf')
-        all_stats = [stats.text.split('\n') for stats in all_stats]
-        match_stats = {stats[1] : (stats[0], stats[2]) for stats in all_stats}
-                
-        final_data = self._get_stats_without_driver(match_stats, driver=driver)
-
-        final_data['home_name'], final_data['away_name'] = self._get_team_names(driver=driver)
-        final_data['season'] = season_idx
-        final_data['match_date'] = self._get_time(driver=driver)
-        final_data['league'], final_data['round'] = self._get_league_and_round(driver=driver)
-        final_data['home_goals'], final_data['away_goals'] = self._get_goals(driver=driver)
-        final_data['result'] = self._get_result(final_data['home_goals'], final_data['away_goals'])
-
-        url = url.replace('statystyki-meczu/0', 'sklady')
-        self.get_website(driver, url)
-
-        final_data['home_formation'], final_data['away_formation'] = self._get_formation(driver=driver)
-        final_data['home_mean_raiting'], final_data['away_mean_raiting'] = self._get_mean_raiting(driver=driver)
-        final_data['home_excluded_count'], final_data['away_excluded_count'] = self._get_excluded_players_count(
-            driver=driver)
-                
-        return final_data
-    
-    @error_handler
-    def _get_stats_without_driver(self, match_stats, *, driver):
-        data = {}
         stats_category = {'Posiadanie piłki': 'poss', 'Oczekiwane bramki (xG)': 'xG', 'Sytuacje bramkowe': 'shots',
                         'Strzały na bramkę': 'acc_shots', 'Strzały niecelne': 'inacc_shots', 'Podania': 'passes',
                         'Rzuty rożne': 'corners', 'Interwencje bramkarzy': 'goalkeeper_saves',
                         'Rzuty wolne': 'free_kicks', 'Spalone': 'offsides', 'Faule' : 'fouls'}
                 
-        for key, value in stats_category.items():
-            if key == 'Podania':
-                data[f'home_{value}'], data[f'home_acc_{value}'], \
-                    data[f'away_{value}'], data[f'away_acc_{value}'] = \
-                        self._get_passes(match_stats)
-                        
-            elif key == 'Posiadanie piłki':
-                data[f'home_{value}'], data[f'away_{value}'] = self._get_possesion(match_stats[key])
+        if self.is_element_present(driver, By.ID, 'onetrust-reject-all-handler'):
+            self.wait_and_click_button(driver, By.ID, 'onetrust-reject-all-handler')
 
-            else:
-                data[f'home_{value}'], data[f'away_{value}'] = self._split_home_and_away(
-                match_stats, key, float if key == 'Oczekiwane bramki (xG)' else int)
+        match_stats = self._scrape_main_stats(driver)
 
-        return data
+        if not match_stats:
+            for value in stats_category.values():
+                final_data[f'home_{value}'] = final_data[f'away_{value}'] = np.nan
+        
+        else:
+            for key, value in stats_category.items():
+                if key == 'Podania':
+                    final_data[f'home_{value}'], final_data[f'home_acc_{value}'], \
+                        final_data[f'away_{value}'], final_data[f'away_acc_{value}'] = self._get_passes(match_stats)
+                                
+                elif key == 'Posiadanie piłki':
+                    if key not in match_stats:
+                        final_data[f'home_{value}'], final_data[f'away_{value}'] = np.nan, np.nan
+                    else:
+                        final_data[f'home_{value}'], final_data[f'away_{value}'] = self._get_possesion(match_stats[key])
 
-    @error_handler
-    def _get_team_names(self, *, driver):
+                else:
+                    final_data[f'home_{value}'], final_data[f'away_{value}'] = self._split_home_and_away(
+                    match_stats, key, float if key == 'Oczekiwane bramki (xG)' else int)
+
+        final_data['home_name'], final_data['away_name'] = self._get_team_names(driver)
+        final_data['season'] = season_idx
+        final_data['match_date'] = self._get_time(driver)
+        final_data['league'], final_data['round'] = self._get_league_and_round(driver)
+        final_data['home_goals'], final_data['away_goals'] = self._get_goals(driver)
+        final_data['result'] = self._get_result(final_data['home_goals'], final_data['away_goals'])
+
+        url = url.replace('statystyki-meczu/0', 'sklady')
+        self.get_website(driver, url)
+
+        if match_stats:
+            funcs = [self._get_formation, self._get_mean_raiting, self._get_excluded_players_count, self._get_coach]
+            values = sum((func(driver) for func in funcs), ())
+        else:
+            values = [np.nan] * 8
+        
+        keys = ['home_formation', 'away_formation', 'home_mean_raiting', 'away_mean_raiting',
+                    'home_excluded_count', 'away_excluded_count', 'home_coach', 'away_coach']
+        final_data.update(dict(zip(keys, values)))
+
+        return final_data
+
+    def _get_coach(self, driver):
+        self.wait_until_element_is_visible(driver, By.CLASS_NAME, 'wcl-mainRow_Xi7Hi')
+        coaches = self.find_elements(driver, By.CLASS_NAME, 'wcl-mainRow_Xi7Hi')
+        coaches = (coaches[-2].text, coaches[-1].text)
+        return coaches
+
+    def _get_team_names(self, driver):
         self.wait_until_element_is_visible(driver, By.CLASS_NAME, 
             'participant__participantName')
         
         names = self.find_elements(driver, By.CLASS_NAME, 'participant__participantName')
         return names[0].text, names[-1].text
     
-    @error_handler
-    def _get_excluded_players_count(self, *, driver):
+    def _get_excluded_players_count(self, driver):
         if not self.is_element_present(
             driver, By.CSS_SELECTOR, '.wcl-caption_xZPDJ.wcl-scores-caption-05_f2TCB.wcl-description_iZZUi'):
             return 0, 0
@@ -131,43 +131,46 @@ class ScrapeStatistics(PageInteractor):
             driver, By.CSS_SELECTOR, '.wcl-caption_xZPDJ.wcl-scores-caption-05_f2TCB.wcl-description_iZZUi')
         excluded = [player.location['x'] for player in excluded]
         home_excluded_count = excluded.count(320)
-        awaay_excluded_count = len(excluded) - home_excluded_count
-        return home_excluded_count, awaay_excluded_count
+        away_excluded_count = len(excluded) - home_excluded_count
+        return int(home_excluded_count), int(away_excluded_count)
 
-    @error_handler
-    def _get_mean_raiting(self, *, driver):
-        self.wait_until_element_is_visible(driver, By.CSS_SELECTOR, '[data-testid="wcl-scores-caption-05"]')
-        raitings = self.find_elements(driver, By.CSS_SELECTOR, '[data-testid="wcl-scores-caption-05"]')
-        return float(raitings[0].text), float(raitings[1].text) 
-
-    @error_handler
-    def _get_formation(self,*, driver):
-        self.wait_until_element_is_visible(driver, By.XPATH,
-        """//*[contains(@class, 'wcl-headerSection') 
-        and contains(@class, 'wcl-text') and contains(@class, 'wcl-spaceBetween')]""")
-
-        formations = self.find_element(
-        driver, By.XPATH, """//*[contains(@class, 'wcl-headerSection') 
-            and contains(@class, 'wcl-text') and contains(@class, 'wcl-spaceBetween')]""").text
+    def _get_mean_raiting(self, driver):
+        if not self.is_element_present(driver, By.XPATH, "//*[contains(@class, 'wcl-badgeRating_1MU6s') and contains(@class, 'lf__teamRatingWrapper--away')]"):
+            return np.nan, np.nan
+            
+        away_raiting = self.find_elements(driver, By.XPATH, "//*[contains(@class, 'wcl-badgeRating_1MU6s') and contains(@class, 'lf__teamRatingWrapper--away')]")[0].text
+        home_raiting = self.find_elements(driver, By.XPATH, "//*[contains(@class, 'wcl-badgeRating_1MU6s') and contains(@class, 'lf__teamRatingWrapper--home')]")[0].text
+        return float(home_raiting), float(away_raiting)
         
-        formations = formations.split(('\n'))
-        formations = ['werf']
-        return formations[0], formations[2]
+    def _get_formation(self, driver):
+        try:
+            self.wait_until_element_is_visible(driver, By.CSS_SELECTOR,
+            '.wcl-headerSection_5507A.wcl-text_F6xdz.wcl-spaceBetween_WGy1W')
+        except TimeoutException:
+            return np.nan, np.nan
+
+        formations = self.find_elements(driver, By.CSS_SELECTOR,
+        '.wcl-headerSection_5507A.wcl-text_F6xdz.wcl-spaceBetween_WGy1W')
+
+        formations = [x.text.split(('\n')) for x in formations]
+
+        if formations[0] == ['KURSY PRZEDMECZOWE']:
+            self.get_website(driver, driver.current_url)
+            return self._get_formation(driver)
+        return formations[0][0], formations[0][2]
     
     @staticmethod
     def _get_result(home, away):
         return (home < away) + (home != away)
     
-    @error_handler
-    def _get_goals(self, *, driver):
+    def _get_goals(self, driver):
         result = self.find_element(driver, By.CLASS_NAME, 'detailScore__wrapper').text
         dash = result.find('-')
         home_goals = int(result[ : dash - 1])
         away_goals = int(result[dash + 2 : ])
         return home_goals, away_goals
 
-    @error_handler
-    def _get_league_and_round(self, *, driver):
+    def _get_league_and_round(self, driver):
         league_and_round = self.find_element(driver, By.CLASS_NAME, 'tournamentHeader__country').text
 
         round_start = league_and_round.find('KOLEJKA')
@@ -179,8 +182,7 @@ class ScrapeStatistics(PageInteractor):
 
         return league_name, league_round
 
-    @error_handler
-    def _get_time(self, *, driver):
+    def _get_time(self, driver):
         self.wait_until_element_is_visible(driver, By.CLASS_NAME, 'duelParticipant__startTime')
         start_time = self.find_element(driver, By.CLASS_NAME, 'duelParticipant__startTime').text
         return start_time
@@ -217,7 +219,8 @@ class ScrapeStatistics(PageInteractor):
                         'excel' : lambda: data.to_excel('stats.xlsx', index=False, engine='openpyxl')}
         file_formats.get(format)()
 
-    def _read_links(self):
+    @staticmethod
+    def _read_links():
         with open('match_links_with_season_indexes.pkl', 'rb') as file:
             return pickle.load(file)
 
@@ -238,8 +241,9 @@ class Worker(threading.Thread):
             match, season_idx = self.task_queue.get()
             if isinstance(match, str):
                 result = self.task(self.driver, match, season_idx)
-                with ScrapeStatistics.lock:
-                    self.results.append(result)
+                if result:
+                    with ScrapeStatistics.lock:
+                        self.results.append(result)
             else:
                 self.saver(data=self.results)
                 self.saver(format='excel')
@@ -247,9 +251,9 @@ class Worker(threading.Thread):
                 
 if __name__ == '__main__':
     start = perf_counter()
-    stats_scraper = ScrapeStatistics(6)
+    stats_scraper = ScrapeStatistics(7)
     stats_scraper.get_all_stats()
     end = perf_counter()
-    print("pobieranie statystyk trwało: ", end - start)
+    print('pobieranie statystyk trwało: ', end - start)
     stats_scraper.save_stats('excel')
     print(stats_scraper.data)
--- page_interactor.py
+++ page_interactor.py
@@ -6,26 +6,31 @@ from selenium.common.exceptions import NoSuchElementException, StaleElementRefer
 class PageInteractor:
     
     def get_website(self, driver, url):
-        driver.get(url)
+        while True:
+            try:
+                driver.get(url)
+                break
+            except:
+                pass
 
     def quit_website(self, driver):
         driver.quit()
 
-    def wait_until_element_is_not_visible(self, driver, by_what, text, time=20):
+    def wait_until_element_is_not_visible(self, driver, by_what, text, time=30):
         WebDriverWait(driver, time).until(
             EC.invisibility_of_element_located((by_what, text)))
         
-    def wait_until_element_is_visible(self, driver, by_what, text, time=20):
-        WebDriverWait(driver, time).until(
-            EC.visibility_of_element_located((by_what, text)))
+    def wait_until_element_is_visible(self, driver, by_what, text, time=30):
+                WebDriverWait(driver, time).until(
+                    EC.visibility_of_element_located((by_what, text)))
 
-    def wait_and_click_button(self, driver, by_what, text, time=20):
+    def wait_and_click_button(self, driver, by_what, text, time=30):
         button = WebDriverWait(driver, time).until(
             EC.element_to_be_clickable((by_what, text)))
         button.click()
         return button
     
-    def wait_until_element_present_on_dom(self, driver, by_what, text, time=20):
+    def wait_until_element_present_on_dom(self, driver, by_what, text, time=30):
         value = WebDriverWait(driver, time).until(
                 EC.presence_of_element_located((by_what, text)))
         return value
